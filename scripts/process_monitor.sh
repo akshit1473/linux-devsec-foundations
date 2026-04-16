d4443c34812afe61798be61e9e098903df4c3123
akshit1473 <akshitjha999999@gmail.com>
2026-04-16 23:47:22 +0530

    Add files via upload

---
+++ scripts/process_monitor.sh
@@ -0,0 +1,37 @@
+#!usr/bin/bash
+
+echo " =================================================="
+echo  "                    PROCESS MONITOR               "
+echo " ================================================="
+echo ""
+echo "[TOP CPU PROCESSES] "
+echo '-----------------------'
+
+printf "%-15s"
+
+ps -eo pid,comm,state,%cpu,%mem --sort=-%cpu |  awk ' 
+NR ==1  {print;next}
+$2 =="ps"  {next}
+$4 >  30 {print "HIGH CPU" , $0}
+$4 < 30 {$0}
+'
+#sort through the processes with the highest cpu usage and  show the top 5.
+
+echo ""
+echo "[TOP MEMORY PROCESSES]"
+echo '-------------------------'
+
+ps -eo pid,comm,state,%cpu,%mem  --sort=-%mem |grep -v ps |  head -n 6
+
+
+echo ""
+echo "[PROCESS STATES]: "
+echo '-----------------------'
+ps -eo pid,comm,state,%cpu,%mem --sort=-%cpu | grep -v ps |  head -n 11
+
+# -eo allows user to basically cherry pick what  fields they want to see
+
+echo "========================================="
+echo  "      PROCESS MONITOR COMPLETE          "
+echo "========================================="
+
