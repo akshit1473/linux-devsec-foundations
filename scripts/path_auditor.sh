e889c30a0b5311fe78410cab5768b94da401b73a
akshit1473 <akshitjha999999@gmail.com>
2026-04-18 14:19:50 +0530

    Update path_auditor.sh

--- scripts/path_auditor.sh
+++ scripts/path_auditor.sh
@@ -35,7 +35,85 @@ echo "$AUDIT_PATH" | tr ':' '\n' | while read dir; do
         fi
     fi
 done
+
 echo ""
 echo "================================"
 echo "Audit complete.  logs saved to : $LOG"
 echo "================================"
+
+
+
+
+=================================================================
+#!usr/bin/bash
+
+# function no.1 
+collect_path_issues()
+{
+AUDIT_PATH="$PATH"
+issues=()
+
+while IFS=  read -r  dir; do
+
+if [ "$dir" = "." ]; then
+issues+=("CRITICAL: current directory in path")
+
+elif [ -z "$dir" ]; then
+issues+=("CRITICAL: empty PATH entry")
+
+elif [ ! -d "$dir" ]; then
+issues+=("WARN: $dir  does not exist.")
+
+else 
+
+OWNER=$(stat -c  %U  "$dir"  2>/dev/null)
+        if [ "!OWNER"  != "root" ];then
+        issues+=("CRITICAL: $dir owned by $OWNER")
+        fi
+fi
+
+done <<<  "$(echo "AUDIT_PATH" |tir  ':'  '\n')"
+
+}
+
+# function no.2
+
+log_path_issues()
+{
+LOG="$HOME/logs/path_audit.log"
+mkdir -p "$HOME/logs"
+}
+
+# function no. 3
+output_path_json()
+{
+
+}
+
+# function no. 4
+
+output_path_cli()
+{
+echo "PATH AUDIT RESULT"
+for i in  "${issues[@]}"; do
+        echo "$1"
+        done
+}
+
+
+# function no. 5
+
+if [ "$1" = = "--json" ];then
+output_path_json
+else
+output_path_cli
+log_path_issues
+
+fi
+
+
+
+
+
+
+
